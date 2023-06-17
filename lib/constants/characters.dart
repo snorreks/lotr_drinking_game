50099f9e025abca8b5510dd1a7d806db21bf25c4
maplesyrup18 <aceasa08@hotmail.com>
2023-06-17 14:58:38 +0200

    Character rules

--- lib/constants/characters.dart
+++ lib/constants/characters.dart
@@ -67,4 +67,53 @@ extension CharacterExtension on Character {
         return 'Gimli';
     }
   }
+
+  List<String> get rules {
+    switch (this) {
+      case Character.frodo:
+        return [
+          'The ring affects his behavior.',
+          'He says, “Oh Sam”.',
+          'He trips over his own feet.'
+        ];
+      case Character.sam:
+        return [
+          'He says, “Mr Frodo”.',
+          "He's mean to Gollum.",
+          'He supports a Frodo who has once again fucked up or is having a Heroic BSoD'
+        ];
+      case Character.merryAndPippin:
+        return [
+          'They cause trouble or create mischief.',
+          'They talk about food.',
+          'They show bravery despite their fear.',
+          'They manage to make a seemingly hopeless situation light-hearted.'
+        ];
+      case Character.gandalf:
+        return [
+          'He gives sage advice or shares a piece of ancient wisdom.',
+          'Drink whenever he mysteriously disappears or appears without clear explanation',
+          'Drink whenever he calls on the name of a higher power like “Elberet, “Mithrandir”, or “Udun”'
+        ];
+      case Character.aragorn:
+        return [
+          'He kills something.',
+          "He's doubtful of his ability to be the rightful king of Gondor.",
+          'He smiles like an idiot'
+        ];
+      case Character.legolas:
+        return [
+          "He uses his bow (extra sip if it's a major action scene).",
+          'He gives a silent, knowing look.',
+          'He interacts with Gimli, displaying their unique friendship.'
+        ];
+      case Character.gimli:
+        return [
+          'He says “dwarf” or “elf”.',
+          "He's used as a comic relief (Beware of the Director's Cut).",
+          'He mentions the skill or strength of dwarves.'
+        ];
+        ;
+    }
+  }
 }
