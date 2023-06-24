4d6620ef3afc7d76eca97b3323b11b0d83a1ab1b
maplesyrup18 <aceasa08@hotmail.com>
2023-06-24 15:41:00 +0200

    Character rules are stored in separate tab

--- lib/ui/views/rules/rules_mobile.dart
+++ lib/ui/views/rules/rules_mobile.dart
@@ -13,7 +13,10 @@ class _RulesMobileState extends State<_RulesMobile> {
   // This assumes the total number of panels to be 4 (for basics, take a drink when, down the hatch, additional rules)
   // + the number of Character.values
   final List<bool> _panelOpen =
-      List<bool>.filled(4 + Character.values.length, false);
+      List<bool>.filled(5 + Character.values.length, false);
+
+  final List<bool> _characterPanelOpen =
+      List<bool>.filled(Character.values.length, false);
 
   @override
   Widget build(BuildContext context) {
@@ -40,15 +43,36 @@ class _RulesMobileState extends State<_RulesMobile> {
                           '${ruleWithName.ruleName}: ${ruleWithName.ruleDescription}')
                       .toList(),
                   3),
-              ...Character.values
-                  .asMap()
-                  .entries
-                  .map((MapEntry<int, Character> entry) {
-                final int index = 4 + entry.key;
-                final Character character = entry.value;
-                return _buildPanel('Character Rules - ${character.displayName}',
-                    character.rules, index);
-              }).toList(),
+              ExpansionPanel(
+                headerBuilder: (BuildContext context, bool isExpanded) {
+                  return ListTile(title: Text('Characters Rules'));
+                },
+                body: Column(
+                  children: Character.values
+                      .asMap()
+                      .entries
+                      .map((MapEntry<int, Character> entry) {
+                    final int index = entry.key;
+                    final Character character = entry.value;
+                    return ExpansionPanelList(
+                      expansionCallback:
+                          (int innerIndex, bool innerIsExpanded) {
+                        setState(() {
+                          _characterPanelOpen[index] = !innerIsExpanded;
+                        });
+                      },
+                      children: [
+                        _buildPanel(
+                            'Character Rules - ${character.displayName}',
+                            character.rules,
+                            index,
+                            isCharacterPanel: true)
+                      ],
+                    );
+                  }).toList(),
+                ),
+                isExpanded: _panelOpen[4],
+              ),
             ],
           ),
         ],
@@ -56,16 +80,18 @@ class _RulesMobileState extends State<_RulesMobile> {
     );
   }
 
-  ExpansionPanel _buildPanel(String header, List<String> body, int index) {
+  ExpansionPanel _buildPanel(String header, List<String> body, int index,
+      {bool isCharacterPanel = false}) {
     return ExpansionPanel(
-      headerBuilder: (BuildContext context, bool isExpanded) {
-        return ListTile(title: Text(header));
-      },
-      body: Column(
-        children:
-            body.map((String item) => ListTile(title: Text(item))).toList(),
-      ),
-      isExpanded: _panelOpen[index],
-    );
+        headerBuilder: (BuildContext context, bool isExpanded) {
+          return ListTile(title: Text(header));
+        },
+        body: Column(
+          children:
+              body.map((String item) => ListTile(title: Text(item))).toList(),
+        ),
+        isExpanded:
+            isCharacterPanel ? _characterPanelOpen[index] : _panelOpen[index],
+        canTapOnHeader: true);
   }
 }
